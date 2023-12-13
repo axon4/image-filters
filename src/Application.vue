a691cb63834c22311bb51125a9d1aa52bd282bbc
axon4 <65050809+axon4@users.noreply.github.com>
2023-12-13 21:16:51 +0000

    install & configure UnPlugIn--Vue-Components, port backGround-template, add backGround

--- src/Application.vue
+++ src/Application.vue
@@ -16,4 +16,5 @@
 			</div>
 		</div>
 	</div>
+	<BackGround />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import { VitePluginFonts } from 'vite-plugin-fonts';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -13,7 +14,8 @@ export default defineConfig({
 					name: 'Quicksand'
 				}]
 			}
-		})
+		}),
+		Components()
 	],
 	resolve: {
 		alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}
