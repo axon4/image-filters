268fa332aeb58b1e6139fe3ab2b8d981e61c3196
axon4 <65050809+axon4@users.noreply.github.com>
2023-12-13 20:54:16 +0000

    install & configure Vite-PlugIn--Fonts, lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { URL, fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		Vue(),
+		VitePluginFonts({
+			google: {
+				families: [{
+					name: 'Quicksand'
+				}]
+			}
+		})
+	],
 	resolve: {
 		alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}
 	}
 });
-
