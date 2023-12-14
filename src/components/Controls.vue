c0b6f3d75c3bf6405ba124002a15a8c2e9d8ca9c
axon4 <65050809+axon4@users.noreply.github.com>
2023-12-14 21:34:26 +0000

    parse filter-names inTo filter-controls, style active filter

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -3,11 +3,16 @@
 		<canvas width='448' height='448' />
 		<div class='text-white text-xl mt-4'>
 			<div class='flex justify-center gap-4'>
-				<button type='button' class='bg-pink-600 py-4 w-full'>Filter</button>
-				<button type='button' class='bg-pink-600 py-4 w-full'>Filter</button>
-				<button type='button' class='bg-pink-600 py-4 w-full'>Filter</button>
+				<button type='button' class='py-4 w-full' v-for='(filter, index) of filters' :key='index' :class='filter === store.filter ? "bg-pink-600" : "bg-green-600"' @click='store.filter = store.filter === filter ? "" : filter'>{{ filter }}</button>
 			</div>
 			<a class='bg-indigo-700 py-4 block w-full mt-2 text-center'>DownLoad</a>
 		</div>
 	</div>
 </template>
+
+<script setup lang='ts'>
+	import useImageStore from '@/stores/image';
+
+	const store = useImageStore();
+	const filters = ['Oceanic', 'Vintage', 'Rose-Tint'];
+</script>
